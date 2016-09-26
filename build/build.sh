4895a12ecd24cd53a91bc72a393dfeb7562db9e3
sbragagnolo <santiagobragagnolo@gmail.com>
2016-09-26 17:37:29 +0200

    moving to dev03

--- build/build.sh
+++ build/build.sh
@@ -10,6 +10,6 @@ wget -O - get.pharo.org/50+vm | bash
 
 echo "Loading Scale and Dependencies..."
 #Load stable version of the monticello configuration, according to this git sources
-./pharo Pharo.image ../build/installScale.st
+./pharo-ui Pharo.image ../build/installScale.st
 
 
